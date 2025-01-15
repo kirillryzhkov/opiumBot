924847b0083247115ee4e6bb0a9af2f56898f599
Kirill <krl.78@mail.ru>
2025-01-15 12:49:01 +0500

    Update bot_tokens.py

--- bot_tokens.py
+++ bot_tokens.py
@@ -1,5 +1,5 @@
-TOKEN = "7981804116:AAGIz41m01LUCF1FHRSgyFmyFlcDPDuOsS0" #для тг бота
+TOKEN = "qweqwe" #для тг бота
 
 # для спотика
-client_id = "f814986eaf0c4a26a48e8c4c0b486f0c"
-client_secret = "110eee39e3c342e797bfcfa7ee2a953f"
+client_id = "qweqwe"
+client_secret = "qweqwe"
